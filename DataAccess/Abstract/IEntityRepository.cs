883ae14585346e070620040a5a541312163a11ac
PASHA011 <abab101083@gmail.com>
2022-07-16 00:34:23 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    interface IEntityRepository<T>where T :class,IEntity,new()
+    {
+        List<T> GetAll();
+    }
+}
