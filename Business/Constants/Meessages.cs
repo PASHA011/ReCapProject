950562cabbaef070c7e8a2a66568ced36fba5737
PASHA011 <abab101083@gmail.com>
2022-07-17 20:32:00 +0300

    Result Added

---
+++ Business/Constants/Meessages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public class Meessages
+    {
+        public static string CarAdded = "Araba Eklendi";
+        public static string CarDeleted = "Araba Silind";
+        public static string CarListed = "Arabalar Listelendi";
+
+    }
+}
